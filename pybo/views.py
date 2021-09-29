c0035cd1328a888de80fc1fe1ceecd3295d60273
snm01234 <park96618@gmail.com>
2021-09-29 12:03:18 +0900

    commit

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date') #작성일시의 역순
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
