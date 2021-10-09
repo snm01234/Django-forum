21e79be04677ed33660f1a92bb1ff60fc3399fe6
snm01234 <park96618@gmail.com>
2021-10-09 22:38:03 +0900

    add vote

--- pybo/models.py
+++ pybo/models.py
@@ -4,22 +4,25 @@ from django.db.models.query_utils import Q
 
 # Create your models here.
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     #질문, 질문 내용, 질문 작성 일시
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')    # 추천인 추가
 
     def __str__(self):
         return self.subject
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_anser')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')    # 추천인 추가
+
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
