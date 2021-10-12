e73c95e04dddd167731cc6d317a006c78ec58992
snm01234 <park96618@gmail.com>
2021-10-12 21:30:45 +0900

    add search

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -8,16 +9,24 @@ def index(request):
     pybo 목록 출력
     """
     # 입력 인자
-    page = request.GET.get('page', 1)
+    page = request.GET.get('page', 1)   #페이지
+    kw = request.GET.get('kw', '')      #검색어
     
     # 조회
     question_list = Question.objects.order_by('-create_date') #작성일시의 역순
-
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |                  #제목 검색
+            Q(content__icontains=kw) |                  #내용 검색
+            Q(author__username__icontains=kw) |         #질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)   #답글 글쓴이 검색
+        ).distinct()
+        
     #페이징 처리
     paginator = Paginator(question_list, 10)
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw} #page와 kw가 추가됨
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
